cce9a44920669cc7d8ce75021c5218e7f68589d1
divagar79 <divagar_79@yahoo.co.in>
2015-08-07 11:50:42 +0530

    Added 4 plots and 4 R files

--- plot4.R
+++ plot4.R
@@ -22,7 +22,7 @@ with(data, {
        ylab="Energy sub metering", xlab="")
   lines(Sub_metering_2~Datetime,col='Red')
   lines(Sub_metering_3~Datetime,col='Blue')
-  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n", adj = 0.09,
          legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
   plot(Global_reactive_power~Datetime, type="l", 
        ylab="Global_reactive_power",xlab="datetime")
